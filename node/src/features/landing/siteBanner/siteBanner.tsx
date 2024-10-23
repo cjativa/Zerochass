c6654dff2655ba7806070fb213c443568335413f
Christopher Jativa <christopher.jativa320@gmail.com>
2024-10-23 02:09:31 -0400

    show login modal only when necessary

--- node/src/features/landing/siteBanner/siteBanner.tsx
+++ node/src/features/landing/siteBanner/siteBanner.tsx
@@ -1,55 +1,66 @@
-import { useContext } from 'react';
+import React, { useContext } from "react";
 
-import { Button } from '../../../components/button/button';
-import { AuthenticationContext } from '../../../components/contexts';
+import { Button } from "../../../components/button/button";
+import { AuthenticationContext } from "../../../components/contexts";
 
 export const SiteBanner = () => {
+  const { isAuthenticated, toggleAuthenticationModal } = useContext(
+    AuthenticationContext
+  );
 
-    const { isAuthenticated, toggleAuthenticationModal } = useContext(AuthenticationContext);
+  React.useEffect(() => {
+    /** Callback that handles checking the keyboard event for Ctrl + AltKey + "L"
+     * to render the authentication modal. Basic means of sign-in protection
+     */
+    const keyboardListenerCallback = (event: KeyboardEvent) => {
+      if (
+        (event.metaKey || event.ctrlKey) &&
+        event.altKey &&
+        event.code === "KeyL"
+      ) {
+        toggleAuthenticationModal();
+      }
+    };
+    document.addEventListener("keydown", keyboardListenerCallback);
+    return () => {
+      document.removeEventListener("keydown", keyboardListenerCallback);
+    };
+  }, []);
 
-    return (
-        <div className="site-banner">
-            <div className="site-banner__inner">
-
-                {/** Banner Heading */}
-                <div className="site-banner__header">
-                    <p className="sb-header">learn. build. and write</p>
-                    <p className="sb-subheader">practical and goal-oriented resources for all by all</p>
-                </div>
-
-                {/** Banner Buttons */}
-                <div className="site-banner__buttons">
-                    <Button
-                        style="secondary"
-                        asLink={true}
-                        path="tutorials"
-                    >
-                        Browse and read
-                    </Button>
-
-                    {/** Display the wrting button */}
-                    {
-                        (isAuthenticated)
-                            // When authenticated, it should link to the write page
-                            ? <Button
-                                style={'primary'}
-                                asLink={true}
-                                path="write"
-
-                            >
-                                Start Writing
-                        </Button>
-
-                            // Otherwise, should display authentication modal
-                            : <Button
-                                style={'primary'}
-                                onClick={toggleAuthenticationModal}
-                            >
-                                Start writing!
-                        </Button>
-                    }
-                </div>
-            </div>
+  return (
+    <div className="site-banner">
+      <div className="site-banner__inner">
+        {/** Banner Heading */}
+        <div className="site-banner__header">
+          <p className="sb-header">learn. build. and write</p>
+          <p className="sb-subheader">
+            practical and goal-oriented resources for all by all
+          </p>
         </div>
-    )
-}
+
+        {/** Banner Buttons */}
+        <div className="site-banner__buttons">
+          <Button style="secondary" asLink={true} path="tutorials">
+            Browse and Read
+          </Button>
+
+          {/** Display the wrting button */}
+          {isAuthenticated ? (
+            // When authenticated, it should link to the write page
+            <Button style={"primary"} asLink={true} path="write">
+              Create a Post
+            </Button>
+          ) : (
+            ""
+            /**
+            // Otherwise, should display authentication modal
+            <Button style={"primary"} onClick={toggleAuthenticationModal}>
+              Start Writing
+            </Button>
+             */
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
