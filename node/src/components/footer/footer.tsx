1acdcbb1f8d0f58ecc19d306f2bcb3b1a660000c
Christopher Jativa <christopher.jativa320@gmail.com>
2024-10-23 02:08:05 -0400

    hide not ready links

--- node/src/components/footer/footer.tsx
+++ node/src/components/footer/footer.tsx
@@ -1,14 +1,15 @@
-
 export const Footer = () => {
-    return (
-        <div className="footer">
-            <div className="legal">
+  return (
+    <div className="footer">
+      <div className="legal">
+        {/**
                 <ul className="legal-top">
                     <li>Privacy</li>
                     <li>Terms</li>
                 </ul>
-                <p>© Zerochass, LLC. All rights reserved.</p>
-            </div>
-        </div>
-    );
+        */}
+        <p>© Zerochass, LLC. All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
